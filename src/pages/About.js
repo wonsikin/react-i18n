b7b63dd3f2d040205facc5df289e3f0e9e28c677
Arthur Wang <fnghwsj@gmail.com>
2019-07-31 09:42:56 +0800

    feat: add Interpolation demo

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 import { withTranslation } from "react-i18next";
-import logo from '../logo.svg';
-import '../App.css';
+import logo from "../logo.svg";
+import "../App.css";
 
 function About({ t }) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{t('about:title')}</p>
+        <p>{t("about:title")}</p>
+        <p>{t("about:I.AM", { name: "Arthur" })}</p>
       </header>
     </div>
   );
 }
 
-export default withTranslation(['about'])(About);
+export default withTranslation(["about"])(About);
